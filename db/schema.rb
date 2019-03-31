e6cec61bcef36d3094c34b8c409c3391426332ee
Lindsey Katz <katz.lindsey@gmail.com>
2019-03-31 16:24:59 -0500

    Migrate tags model and generate tags controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_211624) do
+ActiveRecord::Schema.define(version: 2019_03_31_212210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 2019_03_31_211624) do
     t.string "comment"
     t.integer "user_id"
     t.integer "attraction_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "tag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
