a4acd82c8399c2ac24fb2f4455fd478ecb4045bc
Lindsey Katz <katz.lindsey@gmail.com>
2019-03-31 16:00:46 -0500

    Migrate categories model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_205242) do
+ActiveRecord::Schema.define(version: 2019_03_31_205934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 2019_03_31_205242) do
   create_table "avatars", force: :cascade do |t|
     t.string "url"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
