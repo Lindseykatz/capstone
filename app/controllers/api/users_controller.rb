ed3df433a82e705831cfd144cf8178fbea36d937
Lindsey Katz <katz.lindsey@gmail.com>
2019-03-31 15:40:46 -0500

    Add password validations, and add users create route and action

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,18 @@
+class Api::UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+      date_of_birth: params[:date_of_birth],
+      avatar: params[:avatar_id]
+    )
+
+    if user.save
+      render json: {message: 'User created successfully'}, status: :created
+    else
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+    end
+  end
+end
